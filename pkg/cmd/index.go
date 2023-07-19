e2486940f1ebaa16af260445064db9e1fe8b27ac
Michael Aldridge <aldridge.mac@gmail.com>
2023-07-19 01:28:12 -0500

    pkg/cmd: Write indexes to disk

--- pkg/cmd/index.go
+++ pkg/cmd/index.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"encoding/json"
+	"os"
 
 	"github.com/the-maldridge/dumbsync/pkg/index"
 
@@ -10,17 +12,23 @@ import (
 
 func init() {
 	rootCmd.AddCommand(indexCmd)
+
+	indexCmd.Flags().StringVarP(&indexCmdFilePath, "index", "i", "dumbsync.json", "Index File")
 }
 
-var indexCmd = &cobra.Command{
-	Use:   "index <path>",
-	Short: "index produces an index of files",
-	Long: `index produces an index rooted at <path> that
+var (
+	indexCmdFilePath string
+
+	indexCmd = &cobra.Command{
+		Use:   "index <path>",
+		Short: "index produces an index of files",
+		Long: `index produces an index rooted at <path> that
 	contains the files and sums necessary to determine if the files
 	have changed remotely.`,
-	Run:  indexCmdRun,
-	Args: cobra.ExactArgs(1),
-}
+		Run:  indexCmdRun,
+		Args: cobra.ExactArgs(1),
+	}
+)
 
 func indexCmdRun(cmd *cobra.Command, args []string) {
 	i := new(index.Indexer)
@@ -33,5 +41,16 @@ func indexCmdRun(cmd *cobra.Command, args []string) {
 
 	for f, h := range idx.Files {
 		fmt.Printf("%X: %s\n", h, f)
+	}
+
+	bytes, err := json.Marshal(idx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := os.WriteFile(indexCmdFilePath, bytes, 0644); err != nil {
+		fmt.Println(err)
+		return
 	}
 }
